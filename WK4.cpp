835fd1cfd4962c6f7b382c6a89464750854f8309
MinaGirgis-AdelaideUni <a1924861@student.adelaide.edu.au>
2026-03-16 22:52:43 +1100

    fixed searches, erases and input validator.

--- WK4.cpp
+++ WK4.cpp
@@ -2,61 +2,83 @@
 #include <string>
 #include <set>
 
+
 class SetManager {
 public:
-	static int ValidateInpt() {
+	static std::string ValidateInpt() {
 		std::string validator;
-		int inpt = NULL;
+		int inpt;
 
 		std::cout << "Please enter an integer value: ";
 
-		while (inpt == NULL) {
+		while (true) {
 			std::cin >> validator;
+
+			if (validator == "q" || validator == "Q") {
+				return validator;
+			}
 			try {
 				inpt = stoi(validator);
+				return std::to_string(inpt);
 			}
 			catch (std::invalid_argument) {
 				std::cout << "Please enter an integer value: ";
 			}
 		}
-		return inpt;
 	}
 
 
-	static int find(std::set<int> inptSet, int searchedParam) {
-		if (inptSet.find(searchedParam) != inptSet.end()) {
+	static bool find(std::set<int>& inptSet, std::string searchedParam) {
+		if (searchedParam == "q" || searchedParam == "Q") {
+			return false;
+		} else if (inptSet.find(stoi(searchedParam)) != inptSet.end()) {
 			std::cout << "found!\n";
-			return *inptSet.find(searchedParam);
+			return true;
 		}
-		return NULL;
+		return false;
 	}
 
-	static void erase(std::set<int> inptSet, int searchedParam) {
-		if (SetManager::find(inptSet, searchedParam) != NULL) {
-			inptSet.erase(searchedParam);
-			std::cout << "Removed!";
-		} else {
+	static void erase(std::set<int>& inptSet, std::string searchedParam) {
+		if (searchedParam == "q" || searchedParam == "Q") {
+			return;
+		}
+		else if (SetManager::find(inptSet, searchedParam) == true) {
+			inptSet.erase(stoi(searchedParam));
+			std::cout << "Removed!\n";
+		}
+		else {
 			std::cout << "Not in Data Set!";
 		}
 	}
 
-	static void toString(std::set<int> inptSet) {
-		std::cout << "your set";
+	static void toString(std::set<int>& inptSet) {
+		std::cout << "your set ";
 		for (int num : inptSet) {
-			std::cout << num<<" ";
+			std::cout << num << " ";
 		}
 	}
-	
+
 
 };
 
 int main() {
 	std::set<int> usrInptSet;
-	usrInptSet = { SetManager::ValidateInpt()};
+	std::string inpt = "";
+
+	while (true) {
+		inpt = SetManager::ValidateInpt();
+
+		if (inpt == "q" || inpt == "Q") {
+			break;
+		};
+		
+		usrInptSet.insert(stoi(inpt));
+	}
+
 	std::cout << "Search for a Number! \n";
 	SetManager::find(usrInptSet, SetManager::ValidateInpt());
 	SetManager::erase(usrInptSet, SetManager::ValidateInpt());
 	SetManager::toString(usrInptSet);
-	
+
 	return 0;
 }
