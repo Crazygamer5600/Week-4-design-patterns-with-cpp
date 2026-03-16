00ff4fa8526c84e0ff51ed4cfbe17f970e516375
MinaGirgis-AdelaideUni <a1924861@student.adelaide.edu.au>
2026-03-16 21:31:16 +1100

    Tostring set;

--- WK4.cpp
+++ WK4.cpp
@@ -21,20 +21,42 @@ public:
 		}
 		return inpt;
 	}
+
+
 	static int find(std::set<int> inptSet, int searchedParam) {
 		if (inptSet.find(searchedParam) != inptSet.end()) {
-			std::cout << "found!";
+			std::cout << "found!\n";
 			return *inptSet.find(searchedParam);
 		}
 		return NULL;
 	}
 
+	static void erase(std::set<int> inptSet, int searchedParam) {
+		if (SetManager::find(inptSet, searchedParam) != NULL) {
+			inptSet.erase(searchedParam);
+			std::cout << "Removed!";
+		} else {
+			std::cout << "Not in Data Set!";
+		}
+	}
+
+	static void toString(std::set<int> inptSet) {
+		std::cout << "your set";
+		for (int num : inptSet) {
+			std::cout << num<<" ";
+		}
+	}
+	
 
 };
 
 int main() {
 	std::set<int> usrInptSet;
 	usrInptSet = { SetManager::ValidateInpt()};
+	std::cout << "Search for a Number! \n";
 	SetManager::find(usrInptSet, SetManager::ValidateInpt());
+	SetManager::erase(usrInptSet, SetManager::ValidateInpt());
+	SetManager::toString(usrInptSet);
+	
 	return 0;
 }
