36174f1f729d405145227c836acd112e7ba9f6e8
MinaGirgis-AdelaideUni <a1924861@student.adelaide.edu.au>
2026-03-16 13:25:53 +1100

    broke input validator into its own class and method for quick access

--- WK4.cpp
+++ WK4.cpp
@@ -2,19 +2,31 @@
 #include <string>
 #include <set>
 
+class Validator {
+public:
+	static int getValidInt() {
+		std::string validator;
+		int inpt = NULL;
+
+		std::cout << "Please enter an integer value: ";
+
+		while (inpt == NULL) {
+			std::cin >> validator;
+
+			try {
+				inpt = stoi(validator);
+			}
+			catch (std::invalid_argument) {
+				std::cout << "Please enter an integer value: ";
+			}
+		}
+		return inpt;
+	}
+
+};
+
 int main() {
 	std::set<int> usrInptSet;
-	int inpt = NULL;
-	std::string validator;
-
-	std::cout << "Please enter an integer value: ";
-	while (inpt == NULL) {
-		std::cin >> validator;
-		try {
-			inpt = stoi(validator);
-		}catch(std::invalid_argument){
-			std::cout << "Please enter an integer value: ";
-		}
-	}
-	
+	usrInptSet = { Validator::getValidInt()};
+	return 0;
 }
